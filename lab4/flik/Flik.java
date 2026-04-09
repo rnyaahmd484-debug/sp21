efb0dc7d5448b7dbba5c991edf9ccd6f5e96439b
Rania <rnyaahmd484@gmail.com>
2026-04-09 15:10:56 +0200

    Fixed syntax error in Flik class

--- lab4/flik/Flik.java
+++ lab4/flik/Flik.java
@@ -1,8 +1,7 @@
 package flik;
 
-/** An Integer tester created by Flik Enterprises.
- * @author Josh Hug
- * */
-public static boolean isSameNumber(Integer a, Integer b) {
-    return a.equals(b);
+public class Flik {
+    public static boolean isSameNumber(int a, int b) {
+        return a == b;
+    }
 }
